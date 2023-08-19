3b52b572de60a64977a43f11eab46eefe3bbeee4
Hang <141760657+Duanghatai-T@users.noreply.github.com>
2023-08-19 23:48:36 +0700

    Add files via upload

---
+++ midproj/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("PlayfairDisplay-Bold.ttf")]
+namespace midproj
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var fp = new FlyoutPage();
+            fp.Flyout = new MenuPage();
+            fp.Detail = new NavigationPage(new MainPage());
+
+
+            MainPage =fp;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ midproj/MenuPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace midproj
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+            homeb.Clicked += Homeb_Clicked;
+            witchespathb.Clicked += Witchespathb_Clicked;
+            modernwitchcraftb.Clicked += Modernwitchcraftb_Clicked;
+            herbsathomeb.Clicked += Herbsathomeb_Clicked;
+            contactb.Clicked += Contactb_Clicked;
+        }
+
+        private void Contactb_Clicked(object sender, EventArgs e)
+        {
+            var sp = Parent as FlyoutPage;
+            sp.Detail = new NavigationPage(new ContactPage());
+            sp.IsPresented = false;
+        }
+
+        private void Herbsathomeb_Clicked(object sender, EventArgs e)
+        {
+            var sp = Parent as FlyoutPage;
+            sp.Detail = new NavigationPage(new Page3());
+            sp.IsPresented = false;
+        }
+
+        private void Modernwitchcraftb_Clicked(object sender, EventArgs e)
+        {
+            var sp = Parent as FlyoutPage;
+            sp.Detail = new NavigationPage(new Page2());
+            sp.IsPresented = false;
+        }
+
+        private void Witchespathb_Clicked(object sender, EventArgs e)
+        {
+            var sp = Parent as FlyoutPage;
+            sp.Detail = new NavigationPage(new Page1());
+            sp.IsPresented = false;
+        }
+
+        private void Homeb_Clicked(object sender, EventArgs e)
+        {
+            var sp = Parent as FlyoutPage;
+            sp.Detail = new NavigationPage(new MainPage());
+            sp.IsPresented = false;
+        }
+    }
+}
